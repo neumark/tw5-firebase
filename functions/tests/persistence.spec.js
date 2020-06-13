f8b26d7c79db6ec2cbd3b336b96dde13cd46b033
Peter Neumark <neumark.peter@gmail.com>
2020-06-13 18:45:24 +0200

    wip

--- functions/tests/persistence.spec.js
+++ functions/tests/persistence.spec.js
@@ -6,14 +6,27 @@ const admin = require('firebase-admin');
 
 let adminInitStub, firestore, persistence;
 
+class FirestoreRef extends Array {
+  // TODO: validate doc / collection based on existing array length
+  doc(name) {
+    name.split("/").forEach(this.push.bind(this));
+    return this;
+  }
+  collection(name) {
+    return this.doc(name);
+  }
+}
+
 test.before(t => {
     // If index.js calls admin.initializeApp at the top of the file,
     // we need to stub it out before requiring index.js. This is because the
     // functions will be executed as a part of the require process.
     // Here we stub admin.initializeApp to be a dummy function that doesn't do anything.
     sinon.stub(admin, 'initializeApp');
-    Object.defineProperty(admin, 'firestore', {get: () => () => sinon.stub(admin, 'firestore').returns({})});
-    debugger;
+    const mockDb = {
+        collection: x => (new FirestoreRef()).collection(x)
+    };
+    Object.defineProperty(admin, 'firestore', {get: () => () => mockDb});
     require('../src/db');
     // Now we can require index.js and save the exports inside a namespace called myFunctions.
     persistence = require('../src/persistence');
