ed776613ff248410fb96d1994d1e80dcc4f69af1
Peter Neumark <neumark.peter@gmail.com>
2020-06-23 14:22:52 +0200

    updated functions/run.sh to enable debugger

--- functions/run.sh
+++ functions/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 DIR="$(dirname "${BASH_SOURCE%/*}")"
-GOOGLE_APPLICATION_CREDENTIALS="$DIR/../service-account-key.json" "$DIR/../node_modules/.bin/firebase" serve --token "$(cat "$DIR/../keys.json" | jq -r .firebaseToken)" --only functions
+GOOGLE_APPLICATION_CREDENTIALS="$DIR/../service-account-key.json" node "$DIR/../node_modules/.bin/firebase" --token "$(cat "$DIR/../keys.json" | jq -r .firebaseToken)" emulators:start --only functions --inspect-functions
