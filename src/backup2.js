3aeb0f4f03bb1a56b743b3dbd795011b01d49d36
Peter Neumark <neumark.peter@gmail.com>
2020-07-15 12:28:42 +0200

    fixing backup script

--- src/backup2.js
+++ src/backup2.js
@@ -1,6 +1,8 @@
 const fetch = require("node-fetch");
 global.fetch = fetch;
 const { loadTiddler } = require('../plugins/neumark/firestore-syncadaptor-client/core.js');
-const HOST=process.argv[2];
-const TOKEN=process.argv[3];
-loadTiddler(HOST, "", TOKEN).then(tiddlers => console.log(JSON.stringify(tiddlers, null, 4)));
+const host=process.argv[2];
+// TODO: this is hardcoded for now
+const wiki = 'pn-wiki'
+const token=process.argv[3];
+loadTiddler({host, wiki}, TOKEN).then(tiddlers => console.log(JSON.stringify(tiddlers, null, 4)));
