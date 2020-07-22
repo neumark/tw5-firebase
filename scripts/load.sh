56b0f70b756cad5dd082a9ef0d7beeece2c6fc89
Peter Neumark <neumark.peter@gmail.com>
2020-07-22 09:11:10 +0200

    move tiddlywiki editions to editions folder

--- scripts/load.sh
+++ scripts/load.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 DIR="${BASH_SOURCE%/*}"
 TIDDLYWIKI="$DIR/../node_modules/.bin/tiddlywiki"
-TIDDLYWIKI_PLUGIN_PATH="$DIR/../plugins" node $TIDDLYWIKI "$DIR/../tw-local" --load $1
+TIDDLYWIKI_PLUGIN_PATH="$DIR/../plugins" node $TIDDLYWIKI "$DIR/../editions/tw-local" --load $1
