9b03d6337a0c414810d5afd785fb5182e82ac6c6
Peter Neumark <neumark.peter@gmail.com>
2020-07-22 14:59:49 +0200

    make scripts more uniform

--- scripts/tiddlers-to-fs.sh
+++ scripts/tiddlers-to-fs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 DIR="${BASH_SOURCE%/*}"
-TIDDLYWIKI="$DIR/../node_modules/.bin/tiddlywiki"
-TIDDLYWIKI_PLUGIN_PATH="$DIR/../plugins" node $TIDDLYWIKI "$DIR/../editions/tw-local" --load $1
+MAIN="$DIR/../node_modules/.bin/tiddlywiki"
+TIDDLYWIKI_PLUGIN_PATH="$DIR/../plugins" node $( [[ ${DEBUG} ]] && echo "--inspect-brk" ) $MAIN "$DIR/../editions/tw-local" --verbose --load $1
