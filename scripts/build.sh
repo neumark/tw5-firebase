74aafd4c3425a1297882c4624cb9c2f62ef6ee34
Peter Neumark <neumark.peter@gmail.com>
2021-01-04 18:09:36 +0100

    build now runs with --verbose

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 DIR="${BASH_SOURCE%/*}"
 TIDDLYWIKI="$DIR/../node_modules/.bin/tiddlywiki"
-TIDDLYWIKI_PLUGIN_PATH="$DIR/../plugins" node $TIDDLYWIKI "$DIR/../tw" --output "$DIR/../public" --build index
+TIDDLYWIKI_PLUGIN_PATH="$DIR/../plugins" node $TIDDLYWIKI "$DIR/../tw" --output "$DIR/../public" --build index --verbose
