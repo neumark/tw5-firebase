83b8bfed2208af1c8c445eeb0ad94b7fbf45be49
Peter Neumark <neumark.peter@gmail.com>
2021-01-04 20:26:48 +0100

    fixed broken token generation (due to API key restrictions)

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 DIR="${BASH_SOURCE%/*}"
 ID_TOKEN="$($DIR/gettoken.sh)"
-HOST="$(cat "$DIR/../etc/config.json" | jq -r ".host")"
+HOST="$(cat "$DIR/../etc/config.json" | jq -r ".apiEndpoint")"
 node "$DIR/../src/backup2.js" "$HOST" "$ID_TOKEN"
--- scripts/gettoken.sh
+++ scripts/gettoken.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-DIR="${BASH_SOURCE%/*}"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # APIKEY is "Browser Key" at eg: https://console.developers.google.com/apis/credentials?pli=1&project=peterneumark-com&folder=&organizationId=
 APIKEY="$(cat "$DIR/../etc/keys.json" | jq -r .apikey)"
 REFRESH_TOKEN="$(cat "$DIR/../etc/keys.json" | jq -r .refreshToken)"
-ID_TOKEN="$(curl -s -X POST "https://securetoken.googleapis.com/v1/token?key=$APIKEY" -H 'Content-Type: application/x-www-form-urlencoded' -d "grant_type=refresh_token&refresh_token=$REFRESH_TOKEN" | jq -r ".id_token")"
+REFERRER="$(cat "$DIR/../etc/config.json" | jq -r .domain)"
+ID_TOKEN="$(curl --referer "$REFERRER" -s -X POST "https://securetoken.googleapis.com/v1/token?key=$APIKEY" -H 'Content-Type: application/x-www-form-urlencoded' -d "grant_type=refresh_token&refresh_token=$REFRESH_TOKEN" | jq -r ".id_token")"
 echo $ID_TOKEN
