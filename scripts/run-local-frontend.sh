200f7b7265f81d30fa1dd1a37c73867b88bae10f
Peter Neumark <neumark.peter@gmail.com>
2021-01-05 07:43:12 +0100

    minor improvements in utility scripts

--- scripts/run-local-frontend.sh
+++ scripts/run-local-frontend.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# get function endpoint URL from firebase debug log, replace wiki.app with wiki-app in URL.
 ENDPOINT="$(cat "$DIR/../functions/firebase-debug.log" | grep 'http function initialized' | cut -d '(' -f 2 | cut -d ')' -f 1 | sed "s/.app/-app/")"
 pushd "$DIR/.."
-#open 'http://localhost:8080/?apiEndpoint=http%3A%2F%2Flocalhost%3A5001%2Fpeterneumark-com%2Feurope-west3%2Fwiki-app%2F'
 open "http://localhost:8080/?apiEndpoint=$ENDPOINT/"
 yarn run http-server
 popd
--- src/backup2.js
+++ src/backup2.js
@@ -2,6 +2,6 @@ const fetch = require("node-fetch");
 global.fetch = fetch;
 const { loadTiddler } = require('../plugins/neumark/firestore-syncadaptor-client/core.js');
 const host=process.argv[2];
-const wiki = 'pn-wiki' // TODO: this is hardcoded for now
+const wiki = require('../etc/config.json').wiki;
 const TOKEN=process.argv[3];
 loadTiddler({host, wiki}, TOKEN).then(tiddlers => console.log(JSON.stringify(tiddlers, null, 4)));
