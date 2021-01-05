e215d1a5443a35ccf1c5d63eabe3dee9e557fb49
Peter Neumark <neumark.peter@gmail.com>
2021-01-05 18:36:34 +0100

    use firebase authdomain as referrer when getting new session token

--- scripts/env.sh
+++ scripts/env.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # APIKEY is "Browser Key" at eg: https://console.developers.google.com/apis/credentials?pli=1&project=peterneumark-com&folder=&organizationId=
 APIKEY="$(cat "$DIR/../etc/config.json" | jq -r ".firebase.apiKey")"
 PROJECT="$(cat "$DIR/../etc/config.json" | jq -r ".firebase.projectId")"
-REFERRER="$(cat "$DIR/../etc/config.json" | jq -r ".wiki.domain")"
+REFERRER="$(cat "$DIR/../etc/config.json" | jq -r ".firebase.authDomain")"
 TOKEN="$(cat "$DIR/../etc/keys.json" | jq -r .firebaseToken)"
 REFRESH_TOKEN="$(cat "$DIR/../etc/keys.json" | jq -r .refreshToken)"
 SERVICE_ACCOUNT_KEY="$DIR/../etc/service-account-key.json"
