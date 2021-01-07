878ee8753b0a691abfa8420a1c75af265970279a
Peter Neumark <neumark.peter@gmail.com>
2021-01-07 10:54:58 +0100

    readme generation

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-## TW5-firebase/README
-
-<a href="./neumark-peter-gmail-com.html" class="tc-tiddlylink tc-tiddlylink-missing">neumark.peter@gmail.com</a>7th January 2021 at 10:38am
-
 # TW5-firebase
 
 TW5-firebase is a <a href="https://firebase.google.com/" class="tc-tiddlylink-external">Google Firebase</a>-hosted version of <a href="https://tiddlywiki.com/" class="tc-tiddlylink-external">TiddlyWiki 5</a>. It uses the FireStore database to save tiddlers, Firebase Auth for signon, Firebase hosting for serving static files and Firebase functions for the tiddler IO API.
--- scripts/renderreadme.sh
+++ scripts/renderreadme.sh
@@ -11,9 +11,9 @@ TIDDLYWIKICLI="$DIR/../editions/tw-node-firestore/main.js"
 ID_TOKEN="$($DIR/gettoken.sh)"
 TOKEN="$ID_TOKEN" tiddlywiki_cli "$DIR/../editions/tw-node-firestore" \
     --output "$TMPDIR" \
-    --render "TW5-firebase/README" "[is[tiddler]addsuffix[.html]]" "text/plain" "$:/pn-wiki/static/templates/static.tiddler.html"
+    --render "TW5-firebase/README" "[is[tiddler]addsuffix[.html]]" "text/plain" "$:/pn-wiki/static/templates/static.readme.html"
 pandoc -f html-native_divs-native_spans -t gfm --wrap=preserve "$TMPDIR/TW5-firebase/README.html" -o "$TMPDIR/README.md"
 
 cp "$TMPDIR/README.md" "$DIR/../"
 
-rm -rf "$TMPDIR"
+# rm -rf "$TMPDIR"
