22384278df1c01f905b6158626b2c5a3e7c4ed26
Peter Neumark <neumark.peter@gmail.com>
2021-01-07 10:41:00 +0100

    fixed renderreadme.sh

--- scripts/renderreadme.sh
+++ scripts/renderreadme.sh
@@ -8,7 +8,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 TMPDIR="$(mktemp -d)"
 TIDDLYWIKICLI="$DIR/../editions/tw-node-firestore/main.js"
-TOKEN="$($DIR/gettoken.sh)" tiddlywiki_cli "$DIR/../editions/tw-node-firestore" --output "$TMPDIR" --build readme
-pandoc -f html-native_divs-native_spans -t gfm --wrap=preserve "$TMPDIR/readme.html" -o "$DIR/../README.md"
+ID_TOKEN="$($DIR/gettoken.sh)"
+TOKEN="$ID_TOKEN" tiddlywiki_cli "$DIR/../editions/tw-node-firestore" \
+    --output "$TMPDIR" \
+    --render "TW5-firebase/README" "[is[tiddler]addsuffix[.html]]" "text/plain" "$:/pn-wiki/static/templates/static.tiddler.html"
+pandoc -f html-native_divs-native_spans -t gfm --wrap=preserve "$TMPDIR/TW5-firebase/README.html" -o "$TMPDIR/README.md"
+
+cp "$TMPDIR/README.md" "$DIR/../"
 
 rm -rf "$TMPDIR"
