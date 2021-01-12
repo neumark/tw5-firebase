e1093c0760820b1034d10b1fe7189b34b206de74
Peter Neumark <neumark.peter@gmail.com>
2021-01-12 14:00:57 +0100

    an image for docs and simplification for setup-gcloud.sh, which only works from the scripts folder anyway

--- scripts/setup-gcloud.sh
+++ scripts/setup-gcloud.sh
@@ -3,9 +3,8 @@
 # Script to set up GCP credentials for working with tools like gsutil.
 # Usage: $. ./setup-gcloud.sh
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # set up env
-. "$DIR/env.sh"
+. "./env.sh"
 
 export CLOUDSDK_CONFIG="$(mktemp -d)"
 pushd "$DIR"
