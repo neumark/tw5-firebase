b779b03158f888f6aecdd4c26d55e8e4311e8f5f
Peter Neumark <neumark.peter@gmail.com>
2021-01-13 12:14:00 +0100

    firebase cli now run from project root

--- scripts/run-local-frontend.sh
+++ scripts/run-local-frontend.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # get function endpoint URL from firebase debug log, replace wiki.app with wiki-app in URL.
-ENDPOINT="$(cat "$DIR/../functions/firebase-debug.log" | grep 'http function initialized' | cut -d '(' -f 2 | cut -d ')' -f 1 | sed "s/.app/-app/")"
+ENDPOINT="$(cat "$DIR/../firebase-debug.log" | grep 'http function initialized' | cut -d '(' -f 2 | cut -d ')' -f 1 | sed "s/.app/-app/")"
 pushd "$DIR/.."
 open "http://localhost:8080/?apiEndpoint=$ENDPOINT/"
 yarn run http-server
