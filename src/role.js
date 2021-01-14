52e21085bf7cefb0584bec1f2b5cc5a681c194f2
Peter Neumark <neumark.peter@gmail.com>
2021-01-14 11:26:09 +0100

    added arg parsing for role.js

--- src/role.js
+++ src/role.js
@@ -1,11 +1,26 @@
 const admin = require("firebase-admin");
 const Firestore = require('@google-cloud/firestore');
-const config = require(process.env.CONFIGPATH);
+const path = require('path');
 const { roleNames, rolesSchema } = require('../functions/src/schema');
 const { getDB } = require('../functions/src/db');
 const {readRoles} = require('../functions/src/role');
 const {getContentValidatingTransformer} = require('../functions/src/persistence');
 const { ROLES_TIDDLER, GLOBAL_SYSTEM_BAG} = require('../functions/src/constants'); 
+
+
+var argv = require('yargs/yargs')(process.argv.slice(2))
+    .usage('Usage: $0 <command> [options]')
+    .command('grant', 'grant a role to a user')
+    .example('$0 grant -role admin -user foo@bar.com', 'grant admin role to foo@bar.com')
+    .describe('role', 'User role')
+    .describe('user', 'User email')
+    .demandCommand(1)
+    .help('h')
+    .alias('h', 'help')
+    .epilog('Find more help at: https://neumark.github.io/tw5-firebase/')
+    .argv;
+
+const config = require(process.env.CONFIGPATH || path.resolve(__dirname, '../etc/config.json'));
 
 admin.initializeApp(config.firebase);
 
@@ -13,9 +28,6 @@ const db = getDB(admin);
 
 const wiki = config.wiki.wikiName;
 const superadminEmail = `superadmin@${wiki}`;
-// TODO
-const role = "admin";
-const email = "neumark.peter@gmail.com";
 
 const roleTransformer = getContentValidatingTransformer(rolesSchema);
 
@@ -24,13 +36,15 @@ const ensurePresent = (arr, elem) => arr.includes(elem) ? arr : arr.concat(elem)
 
 const addUser = (roles, role, email) => Object.assign({}, roles, {[role]: ensurePresent(roles[role] || [], email)});
 
+const COMMANDS = {
+    grant: async ({role, email}) => await db.runTransaction(async transaction => {
+        return await _grant(db, transaction, role, email, wiki);
+    })
+};
+
 // grant role to email on wiki
-const grant = async (db, transaction, role, email, wiki) => await roleTransformer(
+const _grant = async (db, transaction, role, email, wiki) => await roleTransformer(
     roles => addUser(roles, role, email),
     db, transaction, superadminEmail, wiki, GLOBAL_SYSTEM_BAG, ROLES_TIDDLER, {});
 
-const doGrant = async () => await db.runTransaction(async transaction => {
-    return await grant(db, transaction, role, email, wiki);
-});
-
-doGrant().then(console.log, console.error);
+COMMANDS[argv['_'][0]](argv).then(console.log, console.error);
