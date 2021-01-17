3b99b7a80fd946159d10e27d7096e44054c3473d
Peter Neumark <neumark.peter@gmail.com>
2021-01-17 20:59:26 +0100

    improvements to role.js util

--- src/role.js
+++ src/role.js
@@ -7,18 +7,7 @@ const {readRoles} = require('../functions/src/role');
 const {getContentValidatingTransformer} = require('../functions/src/persistence');
 const { ROLES_TIDDLER, GLOBAL_SYSTEM_BAG} = require('../functions/src/constants'); 
 
-
-var argv = require('yargs/yargs')(process.argv.slice(2))
-    .usage('Usage: $0 <command> [options]')
-    .command('grant', 'grant a role to a user')
-    .example('$0 grant -role admin -user foo@bar.com', 'grant admin role to foo@bar.com')
-    .describe('role', 'User role')
-    .describe('user', 'User email')
-    .demandCommand(1)
-    .help('h')
-    .alias('h', 'help')
-    .epilog('Find more help at: https://neumark.github.io/tw5-firebase/')
-    .argv;
+const ROLE_ACTIONS = ['grant', 'revoke'];
 
 const config = require(process.env.CONFIGPATH || path.resolve(__dirname, '../etc/config.json'));
 
@@ -33,18 +22,52 @@ const roleTransformer = getContentValidatingTransformer(rolesSchema);
 
 // ensure array
 const ensurePresent = (arr, elem) => arr.includes(elem) ? arr : arr.concat(elem);
+const ensureMissing = (arr, elem) => arr.filter(e => e !== elem);
 
-const addUser = (roles, role, email) => Object.assign({}, roles, {[role]: ensurePresent(roles[role] || [], email)});
 
-const COMMANDS = {
-    grant: async ({role, email}) => await db.runTransaction(async transaction => {
-        return await _grant(db, transaction, role, email, wiki);
-    })
-};
+const userRole = (roles, role, email, action='grant') => Object.assign(
+    {},
+    roles,
+    {[role]: (action === 'grant' ? ensurePresent : ensureMissing)(roles[role] || [], email)});
 
 // grant role to email on wiki
-const _grant = async (db, transaction, role, email, wiki) => await roleTransformer(
-    roles => addUser(roles, role, email),
+const _grant = async (db, transaction, role, email, wiki, action) => await roleTransformer(
+    roles => userRole(roles, role, email, action),
     db, transaction, superadminEmail, wiki, GLOBAL_SYSTEM_BAG, ROLES_TIDDLER, {});
 
-COMMANDS[argv['_'][0]](argv).then(console.log, console.error);
+const argv = require('yargs/yargs')(process.argv.slice(2))
+    .usage('Usage: $0 <command> [options]')
+    .command({
+        command: 'role <userid|email> [role] [action]',
+        desc: 'grant a role to a user',
+        builder: yargs => { yargs
+            .positional('userid', {
+            describe: 'User id or email address',
+            type: 'string'})
+            .positional('role', {
+            describe: 'Role to assign',
+            type: 'string',
+            choices: roleNames,
+            default: 'admin'})
+            .positional('action', {
+            describe: 'User role to assign / revoke',
+            type: 'string',
+            choices: ROLE_ACTIONS,
+            default: 'grant'})
+        },
+        handler: argv => {
+            if (!roleNames.includes(argv.role)) {
+                throw new Error('invalid role '+argv.role);
+            }
+            const grantFn = async ({role, email, action}) => await db.runTransaction(
+                async transaction => await _grant(db, transaction, role, email, wiki, action));
+            grantFn(argv).then(console.log, console.error);
+        }
+    })
+    .example('$0 role foo@bar.com', 'grant admin role to foo@bar.com')
+    .example('$0 role foo@bar.com admin revoke', 'revoke admin role from foo@bar.com')
+    .help()
+    .wrap(72)
+    .alias('h', 'help')
+    .epilog('Find more help at: https://neumark.github.io/tw5-firebase/')
+    .argv;
