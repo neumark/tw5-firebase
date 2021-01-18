2de63510d2c95733a635fc1f4402c448e58e532c
Peter Neumark <neumark.peter@gmail.com>
2021-01-18 14:41:44 +0100

    added import command to src/role.js

--- src/role.js
+++ src/role.js
@@ -1,10 +1,11 @@
+const fs = require('fs');
 const admin = require("firebase-admin");
 const Firestore = require('@google-cloud/firestore');
 const path = require('path');
 const { roleNames, rolesSchema } = require('../functions/src/schema');
 const { getDB } = require('../functions/src/db');
 const {readRoles} = require('../functions/src/role');
-const {getContentValidatingTransformer} = require('../functions/src/persistence');
+const {getContentValidatingTransformer, writeTiddler} = require('../functions/src/persistence');
 const { ROLES_TIDDLER, GLOBAL_SYSTEM_BAG} = require('../functions/src/constants'); 
 
 const ROLE_ACTIONS = ['grant', 'revoke'];
@@ -15,8 +16,9 @@ admin.initializeApp(config.firebase);
 
 const db = getDB(admin);
 
-const wiki = config.wiki.wikiName;
-const superadminEmail = `superadmin@${wiki}`;
+const defaultWiki = config.wiki.wikiName;
+
+const DEFAULT_BAG = 'content';
 
 const roleTransformer = getContentValidatingTransformer(rolesSchema);
 
@@ -30,13 +32,21 @@ const userRole = (roles, role, email, action='grant') => Object.assign(
     roles,
     {[role]: (action === 'grant' ? ensurePresent : ensureMissing)(roles[role] || [], email)});
 
+const superadminEmail = wiki => `superadmin@${wiki}`;
+
 // grant role to email on wiki
-const _grant = async (db, transaction, role, email, wiki, action) => await roleTransformer(
+const grantOrRevoke = async (db, transaction, role, email, wiki, action) => await roleTransformer(
     roles => userRole(roles, role, email, action),
-    db, transaction, superadminEmail, wiki, GLOBAL_SYSTEM_BAG, ROLES_TIDDLER, {});
+    db, transaction, superadminEmail(wiki), wiki, GLOBAL_SYSTEM_BAG, ROLES_TIDDLER, {});
+
+const readFile = f => JSON.parse(fs.readFileSync(f.toString()));
 
 const argv = require('yargs/yargs')(process.argv.slice(2))
     .usage('Usage: $0 <command> [options]')
+    .alias('w', 'wiki')
+    .nargs('w', 1)
+    .describe('w', 'name of wiki to operate on')
+    .default('w', defaultWiki)
     .command({
         command: 'role <userid|email> [role] [action]',
         desc: 'grant a role to a user',
@@ -59,9 +69,27 @@ const argv = require('yargs/yargs')(process.argv.slice(2))
             if (!roleNames.includes(argv.role)) {
                 throw new Error('invalid role '+argv.role);
             }
-            const grantFn = async ({role, email, action}) => await db.runTransaction(
-                async transaction => await _grant(db, transaction, role, email, wiki, action));
+            const grantFn = async ({role, email, action, wiki}) => await db.runTransaction(
+                async transaction => await grantOrRevoke(db, transaction, role, email, wiki, action));
             grantFn(argv).then(console.log, console.error);
+        }
+    })
+    .command({
+        command: 'import <tiddlers..>',
+        desc: 'grant a role to a user',
+        builder: yargs => { yargs
+            .positional('tiddlers', {
+            describe: 'JSON file(s) with tiddlers',
+            type: 'string'})
+        },
+        handler: async ({wiki, tiddlers}) => {
+            const allTiddlers = tiddlers.reduce((acc, tiddlerFile) => acc.concat(readFile(tiddlerFile)), []);
+            for (tiddler of allTiddlers) {
+                await db.runTransaction(
+                    async transaction => {
+                        await writeTiddler(db, transaction, superadminEmail(wiki), wiki, tiddler.bag || DEFAULT_BAG, tiddler);
+                    });
+            }
         }
     })
     .example('$0 role foo@bar.com', 'grant admin role to foo@bar.com')
